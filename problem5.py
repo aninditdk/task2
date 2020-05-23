0570f252adbed1fcd53bb7a65d43257d2744ac69
aninditdk <64904988+aninditdk@users.noreply.github.com>
2020-05-23 14:14:59 +0530

    Create problem5.py

---
+++ problem5.py
@@ -0,0 +1,6 @@
+#Given two Python sets, update first set with items that exist only in the first set and not in the second set
+
+S1={1,2,3,4,5,6,7,8,9}
+S2={1,3,4,6,8,11,22,34,51,67}
+S1=S1-S2
+print(S1)
